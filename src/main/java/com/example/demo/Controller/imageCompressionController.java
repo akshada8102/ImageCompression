9ebd3ec637e82b14e5b1b89a6a0802902f99f768
shubs-code <shubs.code@gmail.com>
2025-02-21 23:51:12 +0530

    fixed bugs

--- src/main/java/com/example/demo/Controller/imageCompressionController.java
+++ src/main/java/com/example/demo/Controller/imageCompressionController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.*;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/api/v1/imagecompression")
@@ -15,11 +17,9 @@ public class imageCompressionController {
 
     @GetMapping
     public String imagecompression(){
-        String excelFilePath = "C://Users/sabal/Download/yourExcelFile.xlsx"; // Change to your actual Excel file path
-        String baseOutputPath = "output"; // Base output directory
+        String excelFilePath = Paths.get(".", "src", "main", "resources", "test.xlsx").toAbsolutePath().toString();
 
-        _imagecompresssionservie.processExcel(excelFilePath, baseOutputPath);
-        _imagecompresssionservie.image("https://unbxd-pim-assets.s3.us-east-1.amazonaws.com/images/cadedcbe922732ec1b1e0f0c510077af/1739298975/A2ZDAAAAAAAAAAAA10-Anthology_FrontView.jpg","M001");
+        _imagecompresssionservie.processExcel(excelFilePath);
      return "SUCCESS";
     }
 
